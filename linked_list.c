87381fa4d15018a1153c35e1bfcac9978da86797
yashhlonkar <yashlonkar7447@gmail.com>
2024-08-17 00:08:17 +0530

    add new line

--- linked_list.c
+++ linked_list.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+g#include<stdio.h>
 #include<stdlib.h>
 
 struct node{
@@ -203,7 +203,7 @@ void reverse_list()
         }
         head = prev;
     }
-
+printf("\nfor practise in git\n");
 }
 void main()
 {
